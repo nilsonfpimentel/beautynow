5ba04baa0fde95b8cbb701f5a3cf1918f6b741c9
Keyson Raphael Acioli da Conceição <keyson.rapha@gmail.com>
2018-11-12 14:01:17 -0300

    activity

---
+++ app/src/main/java/kn/beautynow/gui/usuario/login.java
@@ -0,0 +1,15 @@
+package kn.beautynow.gui.usuario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kn.beautynow.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
